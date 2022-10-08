6aee755acb4b865261937843dbdc2296aa6334d4
Derek Truong <derektruongdev@gmail.com>
2022-10-08 12:25:19 -0500

    completed day1 Huntober!

--- string week/day1.js
+++ string week/day1.js
@@ -15,9 +15,52 @@ As an example, here is how a string looks before and after the characters are fi
 
 
 "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770." 
-// implement fix function -> 
 "PRO-TIP #498: IT'S NICE TO SAY HELLO."
 Please create a function that will fix a given string of this mistake before incalculable damage can be done!
 
 If you want to get a head start on untangling this week's message, check out my Huntober Twitter account where I've shared the full input string already.
 */
+
+//P Given a non empty string
+//R a string
+//E "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770." => "PRO-TIP #498: IT'S NICE TO SAY HELLO."
+//P
+//Create object that holds "puzzle key".
+// Declare a empty string.
+// Iterate through the string.
+  // if it is a key => concat value to string variable.
+  // if it is a value => concat key to string variable.
+  // else concat char to string variable
+//return empty string variable.
+
+function decode(string) {
+  let final = "";
+  const INSTRUTIONS = {
+    0: 'O',
+    1: 'I',
+    2: 'Z',
+    3: 'E',
+    4: 'h',
+    5: 'S',
+    6: 'G',
+    7: 'L',
+    8: 'B',
+    9: 'q'
+  }
+
+  for (let char of string) {
+    let keys = Object.keys(INSTRUTIONS);
+    let values = Object.values(INSTRUTIONS);
+
+    if(keys.includes(char)) {
+      final += values[keys.indexOf(char)];
+    } else if (values.includes(char)) {
+      final += keys[values.indexOf(char)];
+    } else {
+      final += char;
+    }
+  }
+  return final;
+}
+console.log(decode("PRO-TIP #498: IT'S NICE TO SAY HELLO."))
+console.log("PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770." )
