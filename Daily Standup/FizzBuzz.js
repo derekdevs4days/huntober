0067fd67381fda488c28b4c45b2938f744f8a0ac
Derek Truong <derektruongdev@gmail.com>
2022-10-12 12:23:17 -0500

    completed day 1

---
+++ Daily Standup/FizzBuzz.js
@@ -0,0 +1,22 @@
+/*
+Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
+*/
+
+// FizzBuzz(15) => 1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz
+
+function FizzBuzz(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+//FizzBuzz(15)
+FizzBuzz(100)
